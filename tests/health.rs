75ce1a23148e2b3b78e8d6bc899bb1e8409924de
Daniel Cunha <danielsoro@gmail.com>
2024-04-26 18:08:01 -0300

    test: changing function order

--- tests/health.rs
+++ tests/health.rs
@@ -1,5 +1,15 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://localhost:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrage
@@ -16,14 +26,4 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-
-    format!("http://localhost:{}", port)
 }
