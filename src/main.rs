6d6374ff1bfabe6b114360d9005767d71c64b67e
Daniel Cunha <danielsoro@gmail.com>
2024-04-28 15:40:59 -0300

    feat: added logs to the app server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuraion");
 
     let pg_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -3,11 +3,13 @@ use crate::routes::subscribe;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use actix_web::middleware::Logger;
 
 pub fn run(listener: TcpListener, pg_pool: PgPool) -> Result<Server, std::io::Error> {
     let pg_pool = web::Data::new(pg_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
             .app_data(pg_pool.clone())
