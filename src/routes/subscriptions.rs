447f1c9e73829510a69eb33ba5fb082d75c0c8e3
Daniel Cunha <danielsoro@gmail.com>
2024-04-30 17:55:40 -0300

    feat: move log implementation to use tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,22 +1,27 @@
 use actix_web::{post, web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::{error_span, info_span, instrument, Instrument};
 use uuid::Uuid;
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct FormData {
     name: String,
     email: String,
 }
 
 #[post("")]
+#[instrument]
 pub async fn subscribes(form: web::Form<FormData>, pg_pool: web::Data<PgPool>) -> impl Responder {
-    log::info!(
-        "Adding '{}' '{}' as a new subscriber",
-        form.name,
-        form.email
+    let request_id = Uuid::new_v4();
+    info_span!(
+            "Adding a new subscriber.",
+            request_id = %request_id,
+            subscriber_email = %form.email,
+            subscriber_name = %form.name
     );
-    log::info!("Saving new subscribe details in the database");
+
+    let query_span = info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -28,16 +33,17 @@ pub async fn subscribes(form: web::Form<FormData>, pg_pool: web::Data<PgPool>) -
         Utc::now()
     )
     .execute(pg_pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            log::info!("New subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            log::error!(
-                "Failed to execute query when saving new subscriber: {:?}",
-                e
+            error_span!(
+                "Failed to execute query when saving new subscriber",
+                error = %e,
+                %request_id,
+                subscriber_email = %form.email,
+                subscriber_name = %form.name
             );
             HttpResponse::InternalServerError().finish()
         }
