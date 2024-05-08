0239c52afffb1248e555c5c84c763d0ee0602c1a
Daniel Cunha <danielsoro@gmail.com>
2024-05-08 15:20:06 -0300

    fix: move request_id to be provided automatically from tracing-actix-web

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,15 @@
 use crate::routes::health;
 use crate::routes::subscribes;
-use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, pg_pool: PgPool) -> Result<Server, std::io::Error> {
     let pg_pool = web::Data::new(pg_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .service(web::scope("health").service(health))
             .service(web::scope("subscriptions").service(subscribes))
             .app_data(pg_pool.clone())
