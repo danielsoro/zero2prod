f893736878d0ae70374aebe261476875b8820767
Daniel Cunha <danielsoro@gmail.com>
2024-05-08 14:04:50 -0300

    refactor: extract sql insert to a new method and improve instrument

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{post, web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
-use tracing::{info_span, Instrument};
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -21,8 +20,21 @@ pub struct FormData {
     )
 )]
 pub async fn subscribes(form: web::Form<FormData>, pg_pool: web::Data<PgPool>) -> impl Responder {
-    let query_span = info_span!("Saving new subscriber details in the database");
-    match sqlx::query!(
+    match insert_subscriber(&pg_pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            tracing::error!("Failed to execute query when saving new subscriber {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(pg_pool, form)
+)]
+pub async fn insert_subscriber(pg_pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -32,14 +44,11 @@ pub async fn subscribes(form: web::Form<FormData>, pg_pool: web::Data<PgPool>) -
         form.name,
         Utc::now()
     )
-    .execute(pg_pool.get_ref())
-    .instrument(query_span)
+    .execute(pg_pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            tracing::error!("Failed to execute query when saving new subscriber {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query {:?}", e);
+        e
+    })?;
+    Ok(())
 }
