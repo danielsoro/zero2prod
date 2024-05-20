0bd2a9f2d17f6d54df7242a92bf9472ef6f583b8
Daniel Cunha <danielsoro@gmail.com>
2024-05-20 19:02:26 -0300

    feat: Added validation of db connection in health

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,6 +1,17 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{get, web, HttpResponse};
+use sqlx::{pool::PoolConnection, PgPool, Postgres};
 
 #[get("")]
-pub async fn health() -> HttpResponse {
-    HttpResponse::NoContent().finish()
+pub async fn health(pg_pool: web::Data<PgPool>) -> HttpResponse {
+    match health_db(&pg_pool).await {
+        Ok(_) => HttpResponse::NoContent().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+async fn health_db(pg_pool: &PgPool) -> Result<PoolConnection<Postgres>, sqlx::Error> {
+    pg_pool.acquire().await.map_err(|e| {
+        tracing::error!("Not able to get connection with database: {}", e);
+        e
+    })
 }
